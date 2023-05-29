6be44a743db6a0ea7983c653e065ebbe1ebab9fa
Piyush Jha <piyush.jha@uwaterloo.ca>
2023-05-29 11:31:11 -0700

    minor changes

--- solve-1.sh
+++ solve-1.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #SBATCH --account=def-vganesh
-#SBATCH --time=1-00:00
+#SBATCH --time=0-01:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-5000
+#SBATCH --array=1-4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=piyush.jha@uwaterloo.ca
+#SBATCH --output=19-files/slurm-%A-%a.out 
 
 module load python/3.8
+source ~/physicscheck/bin/activate
 
-./solve-single-cube.sh 23 constraints_23_c_100000_2_2_0_final.simp 41.cubes $SLURM_ARRAY_TASK_ID
+./solve-single-cube.sh 19 constraints_19_c_100000_2_2_0_final.simp 19_4.cubes $SLURM_ARRAY_TASK_ID
+
+# Checklist: out dir, order, constraint file, cube file
