0df13feee279aae114d689eda8eb7de581e2c2e9
Zhengyu Li <brian.li@uwaterloo.ca>
2023-08-27 17:33:51 -0400

    Update proof-module.sh

--- proof-module.sh
+++ proof-module.sh
@@ -5,7 +5,7 @@ f=$2 #input file
 o=$3 #output .verify file
 
 # Verify DRAT proof
-./drat-trim/drat-trim $f $f.drat -f | tee $o
+./drat-trim/drat-trim $f $f.drat | tee $o
 if ! grep -E "s DERIVATION|s VERIFIED" -q $o
 then
 	echo "ERROR: Proof not verified"
