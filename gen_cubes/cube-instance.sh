56c77f929beb38664c76483f7393c4fb7d44f887
Zhengyu Li <zhengyu@gra-login1.graham.sharcnet>
2023-08-30 09:56:58 -0400

    update cube-instance

--- gen_cubes/cube-instance.sh
+++ gen_cubes/cube-instance.sh
@@ -31,11 +31,10 @@ echo "Processing $cubeline..."
 if [ -z $s ] || [ "$s" == "-m" ] || (( i == 1 ))
 then
 	# Adjoin the literals in the current cube to the instance and simplify the resulting instance with CaDiCaL
-	command="./gen_cubes/apply.sh $f $dir/$((i-1)).cubes $c > $dir/$((i-1)).cubes$c && ./cadical/build/cadical $dir/$((i-1)).cubes$c $dir/$((i-1)).cubes$c.drat -o $dir/$((i-1)).cubes$c.simp -e $dir/$((i-1)).cubes$c.ext -n -c 10000 > $logdir/$((i-1)).cubes$c.simp"
+	./gen_cubes/apply.sh $f $dir/$((i-1)).cubes $c > $dir/$((i-1)).cubes$c
+	command="./cadical/build/cadical $dir/$((i-1)).cubes$c $dir/$((i-1)).cubes$c.drat -o $dir/$((i-1)).cubes$c.simp -e $dir/$((i-1)).cubes$c.ext -n -c 10000 > $logdir/$((i-1)).cubes$c.simp"
 	echo $command
 	eval $command
-	./proof-module.sh $n $dir/$((i-1)).cubes$c log/$((i-1)).cubes$c.verify
-
 	if [ "$s" == "-m" ]
 	then
 		# Run MapleSAT for 10000 conflicts to check if unsatisfiability can be discovered
@@ -71,36 +70,9 @@ else
 	l=$(grep -n "$parentcube" $dir/$((i-2)).cubes | cut -d':' -f1)
 
 	# Adjoin the literals in the current cube to the simplified parent instance and simplify the resulting instance with CaDiCaL
-	command="./gen_cubes/concat-edge-and-apply.sh $n $dir/$((i-2)).cubes$l.simp $dir/$((i-2)).cubes$l.ext $dir/$((i-1)).cubes $c > $dir/$((i-1)).cubes && ./cadical/build/cadical $dir/$((i-1)).cubes $dir/$((i-1)).cubes.drat -o $dir/$((i-1)).cubes$c.simp -e $dir/$((i-1)).cubes$c.ext -n -c 10000 > $logdir/$((i-1)).cubes$c.simp"
+	command="./gen_cubes/concat-edge-and-apply.sh $n $dir/$((i-2)).cubes$l.simp $dir/$((i-2)).cubes$l.ext $dir/$((i-1)).cubes $c | ./cadical/build/cadical -o $dir/$((i-1)).cubes$c.simp -e $dir/$((i-1)).cubes$c.ext -n -c 10000 > $logdir/$((i-1)).cubes$c.simp"
 	echo $command
 	eval $command
-	./proof-module.sh $n $dir/$((i-1)).cubes$c log/$((i-1)).cubes$c.verify
-
-	if [ "$s" == "-b" ]
-	then
-		# Run MapleSAT for 10000 conflicts and output noncanonical blocking clauses
-		rm $dir/$((i-1)).cubes$c.noncanon 2> /dev/null
-		command="./gen_cubes/concat-edge.sh $m $dir/$((i-1)).cubes$c.simp $dir/$((i-1)).cubes$c.ext | ./maplesat-ks/simp/maplesat_static -order=$n -exhaustive=$dir/$((i-1)).cubes$c.exhaust -keep-blocking=2 -noncanonical-out=$dir/$((i-1)).cubes$c.noncanon -max-conflicts=10000 > $logdir/$((i-1)).cubes$c.ncgen"
-		echo $command
-		eval $command
-		printf "Adding %d noncanonical blocking clauses into instance\n" $(wc -l < $dir/$((i-1)).cubes$c.noncanon)
-		if grep -q "UNSATISFIABLE" $logdir/$((i-1)).cubes$c.ncgen
-		then
-			numsols=$(wc -l < $dir/$((i-1)).cubes$c.exhaust)
-			echo "Instance was determined to have ${numsols} solutions by MapleSAT"
-			# If MapleSAT determines there are no solutions make the instance trivially unsatisfiable
-			if [ $numsols -eq 0 ]
-			then
-				echo "0" >> $dir/$((i-1)).cubes$c.simp
-			fi
-		fi
-		rm $dir/$((i-1)).cubes$c.exhaust
-
-		# Add noncanonical blocking clauses into simplified instance
-		mv $dir/$((i-1)).cubes$c.simp $dir/$((i-1)).cubes$c.simp-tmp
-		./gen_cubes/concat.sh $dir/$((i-1)).cubes$c.simp-tmp $dir/$((i-1)).cubes$c.noncanon > $dir/$((i-1)).cubes$c.simp
-		rm $dir/$((i-1)).cubes$c.simp-tmp
-	fi
 fi
 
 # Check if simplified instance was unsatisfiable
@@ -111,13 +83,6 @@ then
 	head $dir/$((i-1)).cubes -n $c | tail -n 1 > $dir/$i-$c.cubes
 	sed 's/a/u/' $dir/$i-$c.cubes -i # Mark cubes that have been shown to be unsatisfiable with 'u'
 	unsat=1
-
-	echo "$dir/$((i-1)).cubes$c.simp is solved by CaDiCaL"
-	
- 	./proof-module.sh $n $dir/$((i-1)).cubes$c log/$((i-1)).cubes$c.verify
-  	
-	#rm $dir/$((i-1)).cubes$c.simp
-	#rm "$dir/$((i-1)).cubes$c.drat"
 else
 	# Determine how many edge variables were removed
 	removedvars=$(sed -E 's/.* 0 [-]*([0-9]*) 0$/\1/' < $dir/$((i-1)).cubes$c.ext | awk "\$0<=$m" | sort | uniq | wc -l)
